696c4cff912f632cb9a44c7417d3f3af2c3cb265
Denise Henderson <denise@henderson3.us>
2020-09-12 15:24:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,11 +30,19 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  # def save
+  #   sql = <<-SQL
+  #     INSERT INTO students(name, grade)
+  #       VALUES (?,?)
+  #   SQL
+  #   DB[:conn].execute(sql, self.name, self.grade)
+  # end
   def save
     sql = <<-SQL
-    INSERT INTO students(name, grade)
-    VALUES (?,?)
+      INSERT INTO students (name, grade)
+        VALUES (?, ?)
     SQL
+
     DB[:conn].execute(sql, self.name, self.grade)
   end
 
