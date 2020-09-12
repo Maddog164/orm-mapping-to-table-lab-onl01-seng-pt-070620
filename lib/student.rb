28c87d13411cb30f3cc15494e4d3617956c6c4cc
Denise Henderson <denise@henderson3.us>
2020-09-12 15:26:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,4 +46,10 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
   end
 
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
 end
